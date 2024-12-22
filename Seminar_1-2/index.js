349f5c12efbc40e550752086a0ddc2b2d147ef94
владимир назаров <volottnazarov@mail.ru>
2024-12-22 21:00:30 +0300

    Добавление, удаление, клонирование элементов

---
+++ Seminar_1-2/index.js
@@ -0,0 +1,36 @@
+﻿let countNum = 0;
+const appBtnEl = document.querySelector('.append');
+const removeBtnEl = document.querySelector('.remove');
+const cloneBtnEl = document.querySelector('.clone');
+const frameEl = document.querySelector('.content');
+
+appBtnEl.addEventListener('click', () => {
+    countNum += 1;
+    const divEl = document.createElement('div');
+    divEl.className = 'box';
+    divEl.setAttribute('id', `${countNum}`);
+    divEl.textContent = `Элемент ${countNum}`;
+    frameEl.appendChild(divEl);
+});
+
+removeBtnEl.addEventListener('click', () => {
+    if(countNum > 0){
+        const boxEl = document.getElementById(`${countNum}`);
+        boxEl.remove();
+        countNum -= 1;
+    }else{
+        alert("Нет элементов для удаления!");
+    }
+});
+
+cloneBtnEl.addEventListener('click', () => {
+    if(countNum > 0){
+        const boxEl = document.getElementById(`${countNum}`);
+        let clone = boxEl.cloneNode(true);
+        countNum += 1;
+        clone.setAttribute('id', `${countNum}`);
+        frameEl.appendChild(clone);
+    }else{
+        alert("Нет елементов для клонирования!");
+    }
+});
